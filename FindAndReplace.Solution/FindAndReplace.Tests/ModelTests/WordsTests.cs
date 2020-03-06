c7edb58af1c5b024e7f806ad39ee20c39404af2a
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-05 16:08:26 -0800

    Add test methods

--- FindAndReplace.Solution/FindAndReplace.Tests/ModelTests/WordsTests.cs
+++ FindAndReplace.Solution/FindAndReplace.Tests/ModelTests/WordsTests.cs
@@ -6,6 +6,34 @@ namespace FindAndReplace.Tests
   [TestClass]
   public class WordsTests
   {
-    // Test methods go here
+
+    [TestMethod]
+    public void WordsConstructor_CreatesInstanceOfWords_Words()
+    {
+      Words newWords = new Words();
+      Assert.AreEqual(typeof(Words), newWords.GetType());
+    }
+
+    [TestMethod]
+    public void WordsConstructor_StoresUserInput_MultipleStrings()
+    {
+      Words newWords = new Words();
+      newWords.FindAndReplace("Hello world", "world", "universe");
+      string sentence = "Hello world";
+      string searchFor = "world";
+      string replaceWith = "universe";
+      Assert.AreEqual(sentence, newWords.Sentence);
+      Assert.AreEqual(searchFor, newWords.SearchFor);
+      Assert.AreEqual(replaceWith, newWords.ReplaceWith);
+    }
+
+    [TestMethod]
+    public void FindAndReplace_SearchForWordAndReplaceWord_String()
+    {
+      Words newWords = new Words();
+      newWords.FindAndReplace("Hello world", "world", "universe");
+      string expectedOutput = "Hello universe";
+      Assert.AreEqual(expectedOutput, newWords.FindAndReplace());
+    }
   }
 }
